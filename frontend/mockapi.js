6658c55489a443860e6984a37d822066c2a0976a
c-apex <juhltommy@gmail.com>
2022-07-13 09:16:26 -0700

    Users may now only access services they own

--- frontend/mockapi.js
+++ frontend/mockapi.js
@@ -10,32 +10,36 @@ app.get('users/auth', function(req, res) {
     else {res.json({"1": 0});}
 });
 
+app.get('/users/apps/Tyler', function(req, res){
+    res.json({"1": ["website 0", "no"], "2": ["ccc"]});
+});
+
 app.get('/users/apps/Daniel', function(req, res){
     res.json({"1": ["website 0", "no"], "2": ["abc", "def"]});
 });
 
 app.get('/users/apps/Tommy', function(req, res){
-    res.json({"1": ["website 1","some sort of API","Just for example pretend this is important..."], "2": ["aaa","bbb","ccc"]});
+    res.json({1: ["website 1","some sort of API","Just for example pretend this is important..."], "2": ["aaa","bbb"]});
 });
 
 app.get('/apps/aaa', function(req, res){
-    res.json({"1": "website 1", 2: "This website is pretty neat, but api 1 is cooler"});
+    res.json({1: "website 1", 2: "This website is pretty neat, but api 1 is cooler", 3: "Tommy"});
 });
 
 app.get('/apps/bbb', function(req, res){
-    res.json({"1": "Number one API EVER", 2: "good apis are cool"});
+    res.json({1: "Number one API EVER", 2: "good apis are cool", 3: "Tommy"});
 });
 
 app.get('/apps/ccc', function(req, res){
-    res.json({"1": "another website...", 2: "Just for example pretend this is important..."});
+    res.json({1: "another website...", 2: "Just for example pretend this is important...", 3: "Tyler"});
 });
 
 app.get('/apps/abc', function(req, res){
-    res.json({"1": "The Zeroth Site", 2: "this website brings us closer to greatness"});
+    res.json({1: "The Zeroth Site", 2: "this website brings us closer to greatness", 3: "Daniel"});
 });
 
 app.get('/apps/def', function(req, res){
-    res.json({"1": "The Danny API", 2: "It's fun to test, trust me."});
+    res.json({1: "The Danny API", 2: "It's fun to test, trust me.", 3: "Daniel"});
 });
 
 app.listen(8081, () => {

797df2ba23466245a38000ee5f8c55baa9efaf82
Tyler Kozlowski <Tyler.J.Kozlowski@gmail.com>
2022-07-19 17:50:57 -0500

    add application POJO with annotations && add user POJO with annotations

---
+++ backend/src/com/dumd/server/monitor/service/dynamodb/models/User.java
@@ -0,0 +1,74 @@
+package com.dumd.server.monitor.service.dynamodb.models;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverted;
+
+import java.util.List;
+
+
+@DynamoDBTable(tableName = "users")
+public class User {
+
+    private String userId;
+    private String customerName;
+    private String email;
+    private String hashedPassword;
+    private String phoneNumber;
+    private List<String> appIds;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "customerName")
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    @DynamoDBAttribute(attributeName = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @DynamoDBAttribute(attributeName = "hashedPassword")
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    @DynamoDBAttribute(attributeName = "phoneNumber")
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @DynamoDBAttribute(attributeName = "appIds")
+    public List<String> getAppIds() {
+        return appIds;
+    }
+
+    public void setAppIds(List<String> appIds) {
+        this.appIds = appIds;
+    }
+}
